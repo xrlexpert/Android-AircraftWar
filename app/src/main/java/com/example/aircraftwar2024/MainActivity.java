ba4897219b18036b92e8951e99b396d5472c1885
xrlexpert <hiroxrl@outlook.com>
2024-05-26 15:33:31 +0800

    update 游戏首页

--- app/src/main/java/com/example/aircraftwar2024/MainActivity.java
+++ app/src/main/java/com/example/aircraftwar2024/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.aircraftwar2024;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RadioGroup musicSelect = findViewById(R.id.musicSelect);
+        RadioButton musicOn = findViewById(R.id.musicOn);
+        RadioButton musicOff = findViewById(R.id.musicOff);
+        musicOff.setChecked(true);
+
+
     }
 
 }
